4889d303e5f0b4205d049ef9c0ae20e109d8ad32
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-28 14:02:14 +0200

    added movies list

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index() {
-        return view('movies');
+        $movies = Movie::all();
+        return view('movies', compact('movies'));
     }
 }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,1 +1,7 @@
-<h3>lista di film</h3>
+@foreach ($movies as $item)
+<h1>{{ $item->title }}</h1>
+<h2>{{ $item->original_title }}</h2>
+<h2>{{ $item->nationality }}</h2>
+<h2>{{ $item->date }}</h2>
+<h2>{{ $item->vote }}</h2>
+@endforeach
