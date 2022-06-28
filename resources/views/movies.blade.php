4393293f926b5ae810f8a7e0c2bec9dd54bee220
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-28 14:24:12 +0200

    added css file

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('main_content') 
 @foreach ($movies as $item)
 <h1>{{ $item->title }}</h1>
 <h2>{{ $item->original_title }}</h2>
@@ -5,3 +8,4 @@
 <h2>{{ $item->date }}</h2>
 <h2>{{ $item->vote }}</h2>
 @endforeach
+@endsection
