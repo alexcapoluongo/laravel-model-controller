5530ddb5d70b12eb27564badda9e7b361e17e3be
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-28 14:34:22 +0200

    added some css deatures

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
 @section('main_content') 
+<main>
+<div class="row">
 @foreach ($movies as $item)
-<h1>{{ $item->title }}</h1>
-<h2>{{ $item->original_title }}</h2>
-<h2>{{ $item->nationality }}</h2>
-<h2>{{ $item->date }}</h2>
-<h2>{{ $item->vote }}</h2>
+    <div class="card">
+        <h1>Title: {{ $item->title }}</h1>
+        <h2>Original title: {{ $item->original_title }}</h2>
+        <p>Nationality: {{ $item->nationality }}</p>
+        <p>Date: {{ $item->date }}</p>
+        <p>Average Vote: {{ $item->vote }}</p>
+    </div>
 @endforeach
+</div>
+<main>
 @endsection
